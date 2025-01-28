08552ff0adeeee32166ba53d45e315c0119ab3b7
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-01-28 18:11:19 +0200

    product by category

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,6 +5,7 @@ import {
   getAllProducts,
   getFeaturedProducts,
   getRecommendedProducts,
+  getProductsByCategory,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
