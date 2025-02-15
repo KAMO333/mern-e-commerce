d1370171588d78cc8b08f93618757866929d564e
Kamogelo <mmopanemoroka@gmail.com>
2025-02-16 01:56:33 +0200

    typo fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = false;
-  const isAdmin = falsesignup;
+  const isAdmin = false;
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
