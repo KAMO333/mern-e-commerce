dbd2d24c0245abbb392dea476295beac56093571
Kamogelo <mmopanemoroka@gmail.com>
2025-05-17 01:49:33 +0200

    redis new database

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 export const redis = new Redis(
   process.env.UPSTASH_REDIS_URL ||
-    "rediss://default:AXOfAAIjcDExZjBkNWU1OGY5N2Q0OTM3YTBhODIwNjQzZTFhYTBkYXAxMA@exotic-pheasant-29599.upstash.io:6379"
+    "rediss://default:AYGeAAIjcDFhNWY1NmUxMzRkMTM0N2NiOTMxNTgwZmRiNGQ3MDNjOHAxMA@knowing-wallaby-33182.upstash.io:6379"
 );
 await redis.set("cash", "money");
