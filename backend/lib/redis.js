56766133a9dfa1cb58312969b3a730bc2b5c6a83
Kamogelo <mmopanemoroka@gmail.com>
2025-05-17 02:19:33 +0200

    changes var

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,14 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const redis = new Redis(
-//   process.env.UPSTASH_REDIS_URL ||
-//     "rediss://default:AXOfAAIjcDExZjBkNWU1OGY5N2Q0OTM3YTBhODIwNjQzZTFhYTBkYXAxMA@exotic-pheasant-29599.upstash.io:6379"
-// );
-
-// await redis.set("cash", "money");
-
-export const client = new Redis(
-  "rediss://default:AYGeAAIjcDFhNWY1NmUxMzRkMTM0N2NiOTMxNTgwZmRiNGQ3MDNjOHAxMA@knowing-wallaby-33182.upstash.io:6379"
+export const redis = new Redis(
+  process.env.UPSTASH_REDIS_URL ||
+    "rediss://default:AYGeAAIjcDFhNWY1NmUxMzRkMTM0N2NiOTMxNTgwZmRiNGQ3MDNjOHAxMA@knowing-wallaby-33182.upstash.io:6379"
 );
-await client.set("foo", "bar");
+
+await redis.set("cash", "money");
