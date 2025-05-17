8840eb8cab591e767de54a9f544e9b15ca725a18
Kamogelo <mmopanemoroka@gmail.com>
2025-05-17 02:03:13 +0200

    redis client

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,8 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(
+// export const redis = new Redis(
+//   process.env.UPSTASH_REDIS_URL ||
+//     "rediss://default:AXOfAAIjcDExZjBkNWU1OGY5N2Q0OTM3YTBhODIwNjQzZTFhYTBkYXAxMA@exotic-pheasant-29599.upstash.io:6379"
+// );
+
+// await redis.set("cash", "money");
+
+export const client = new Redis(
   process.env.UPSTASH_REDIS_URL ||
     "rediss://default:AYGeAAIjcDFhNWY1NmUxMzRkMTM0N2NiOTMxNTgwZmRiNGQ3MDNjOHAxMA@knowing-wallaby-33182.upstash.io:6379"
 );
-await redis.set("cash", "money");
+await client.set("foo", "bar");
