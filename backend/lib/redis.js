58ffec65cf19560f679a946aebbfad8271fa5936
Kamogelo Mmopane <kammopajhb024@student.wethinkcode.co.za>
2025-10-17 14:44:26 +0200

    update Redis connection URL to new Upstash instance

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const redis = new Redis(
   process.env.UPSTASH_REDIS_URL ||
-    "rediss://default:AYGeAAIjcDFhNWY1NmUxMzRkMTM0N2NiOTMxNTgwZmRiNGQ3MDNjOHAxMA@knowing-wallaby-33182.upstash.io:6379"
+    "rediss://default:AWOqAAIncDJmMWQzMDBiOTZhYzI0OWQ1OTAzNzdmYjVkMDc2YTEwNnAyMjU1MTQ@obliging-gannet-25514.upstash.io:6379"
 );
 
 await redis.set("cash", "money");
